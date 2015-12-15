b6128fbcfe7e65ad3c23f1de7a99656e0df65698
jisantuc <james.santucci@gmail.com>
2015-12-15 17:53:29 -0500

    Minimum working example completed
    `scorer.py` accepts command line arguments with verified accurate
    weighted mean scores based on user importance ratings and also accepts a
    config file to specify the same options.
    
    Also wrote example config file.

--- README.md
+++ README.md
@@ -5,33 +5,35 @@ Makin' Scores Easy
 
 This project allows anyone to create custom scores for economics institutions based on their personal preferences about fields of study. There are two ways to use it. The first is from the command line with manually specified options, and the second uses config files. Maybe someday this will be available as a service using Heroku or something, but I'm not a magician, so whatever.
 
+You'll probably want to set up a `virtualenv` for installing depdencies. If you don't know what that is or if you don't know how to do that, follow the `virtualenv` instructions [here](http://docs.python-guide.org/en/latest/dev/virtualenvs/). If you don't have `pip` installed, do that first.
+
 # Usage
 
 ## Command line with options
 
 To use the tool from the command line, first clone this directory using:
 
-`git clone https://github.com/jisantuc/projname.git`
-
-Then `chmod a+x scorer.py`.
+`git clone https://github.com/jisantuc/MakinScoresEasy.git
 
 The options scorer accepts are:
 
-- `--codes`, `-c`: list of [fields](https://ideas.repec.org/top/) (search "Top institutions by field" for a list of possible fields). Should be lowercase, but if you enter them upper case, the scorer will call you names but still work. If you enter no codes, the scorer will assume you want overall rankings.
+- `--codes`, `-c`: list of [fields](https://ideas.repec.org/top/) (search "Top institutions by field" for a list of possible fields). If you enter no codes, the scorer will assume you want overall rankings. Codes used are the New Economics Papers codes found [here](http://nep.repec.org/).
 - `--weights`, `-w`: list of identical length to the list of fields. This option allows you to express, for example, that you care twice as much about corporate finance (CFN) as how much you care about health economics (HEA). These don't need to sum to one, because the scorer will normalize whatever weights you put in.
 - `--outf`, `-o`: an output file. If specified, the results of your custom weighting will be written to disk at the loction specified.
 
 For example, to get a combined financial development and growth (FDG) + heterodox microeconomics (HME) score where you care three times as much about HME as you do about FDG and want to save the results to disk at `mysupergreatrankings.csv`, you would call:
 
 ```bash
-./scorer.py --codes hme fdg --weights 3 1 --outf mysupergreatrankings.csv
+python scorer.py --codes hme fdg --weights 3 1 --outf mysupergreatrankings.csv
 ```
 
 ## Using a config file
 
 Alternatively, you can use a config file. Your config file should have a section called "RePEC Scorer". An example config file is included in this directory under `config.cfg`.
 
-To call using a config file, specify the `--config` option with the path to your config file when you run. **Specifying a config file will cause the scorer to ignore whatever else you specify from the command line.**
+To call using a config file, specify the `--config` option with the path to your config file when you run. Items in a list should be separated by a "`, `" (a comma *and* a space). **Specifying a config file will cause the scorer to ignore whatever else you specify from the command line.**
+
+If you use a config file, you *must* specify a weight for each code.
 
 An example call identical to the above (using the provided config file) would be:
 
--- scorer.py
+++ scorer.py
@@ -1,26 +1,28 @@
 import ConfigParser
 import argparse
+import re
 import pandas as pd
 
 parser = argparse.ArgumentParser(
-    help=('Create weighted institutional rankings from RePEC'
+    description=('Create weighted institutional rankings from RePEC'
           ' based on custom combinations of economic fields')
 )
-parser.add_argument('--codes', '-c', nargs='*',
-                    description='Fields to search')
-parser.add_argument('--weights', '-w', nargs='*',
-                    description=('Weights to assign to scores in '
+parser.add_argument('--codes', '-c', nargs='*', type=str, default=[],
+                    help='Fields to search')
+parser.add_argument('--weights', '-w', nargs='*', type=float, default=[],
+                    help=('Weights to assign to scores in '
                                  'each fields. Must match length '
                                  'of --codes and --codes must be '
                                  'specified'))
 parser.add_argument('--outf', '-o', default=None,
-                    description=('File to dump output. If '
+                    help=('File to dump output. If '
                                  'None, writes to stdout'))
 parser.add_argument('--config',
-                    description=('Location of config file. Ignores other '
+                    help=('Location of config file. Ignores other '
                                  'options if specified.'))
 
-def table_for_code(code):
+
+def table_for_code(code, weight=1):
     """
     Parameters
     ==========
@@ -32,7 +34,16 @@ def table_for_code(code):
     Top rankings from code listed as a pandas DataFrame
     """
 
-    pass
+    base_url = 'https://ideas.repec.org/top/top.%s.html'
+    scoresdf = pd.read_html(base_url % code.lower(),
+                            encoding='utf-8')[0]
+    scoresdf.set_index('Institution', inplace=True)
+    scoresdf['weight'] = weight
+    scoresdf['weighted_score'] = scoresdf['weight'] * scoresdf['Score']
+    return scoresdf[
+        ['Score', 'Authors', 'Rank', 'weight', 'weighted_score']
+    ]
+
 
 def weighted_scores(codes, weights=None):
     """
@@ -57,4 +68,65 @@ def weighted_scores(codes, weights=None):
     Table of weighted scores for each institution as pandas DataFrame
     """
     
-    pass
+    # should be: join + suffixes? merge + suffixes? how does mult-join
+    # work again?
+    # alt: make single code score return a dict of {code: df} and use
+    # keys for suffixes
+
+    dfs = {code: table_for_code(code, weight) for code, weight in 
+           zip(codes, weights)}
+    indices = []
+    indices = list(reduce(lambda x, y: x | y,
+                          map(lambda x: set(x.index.tolist()), dfs.values())))
+    dfs = {code: dfs[code].reindex(indices).fillna(dfs[code]['Score'].max())
+           for code in dfs.keys()}
+    for val in dfs.values():
+        print val.columns
+
+    df = reduce(lambda x, y: x[['weighted_score']] + y[['weighted_score']],
+                dfs.values())
+    return df / sum(weights)
+
+
+def read_config(config_file):
+    parser = ConfigParser.ConfigParser()
+    parser.read(config_file)
+    section = 'ScoringSettings'
+    return {
+        'codes': parser.get(section, 'codes').split(', '),
+        'weights': map(float, parser.get(section, 'weights').split(', ')),
+        'outf': parser.get(section, 'outf')
+    }
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    if args.config:
+        opts = read_config(args.config)
+        codes = opts['codes']
+        weights = opts['weights']
+        outf = opts['outf']
+    else:
+        codes = args.codes if len(args.codes) > 0 else ['inst.all']
+        weights = args.weights if len(args.weights) > 0 else [1] * len(codes)
+        outf = args.outf if args.outf else 'stdout'
+
+    if len(weights) != len(codes):
+        raise ValueError(
+            ('Number of weights passed (%d) must equal number of codes '
+             '(%d)') % (len(args.weights), len(args.codes))
+        )
+
+    scores = weighted_scores(codes, weights)\
+        .sort_values('weighted_score').round(3)
+
+    if outf != 'stdout':
+        scores.to_csv(outf, encoding='utf-8', header=True)
+    else:
+        with pd.option_context('max_rows', len(scores)):
+            print scores
+        if len(scores) > pd.get_option('max_row'):
+            print ('***********************************\n'
+                   'Result df is pretty long. Consider writing to file. Be '
+                   'prepared to do some scrolling.\n'
+                   '***********************************')
